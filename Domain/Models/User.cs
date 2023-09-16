cdd59bbcd39ba2d16ea92e9142928ad405e8cc2f
Muhammadali <qodirovmuhammadali246@gmail.com>
2023-09-16 21:18:59 +0500

    add prop name in class user

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -8,6 +8,7 @@ namespace Domain.Models
 {
     public  class User
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        public string  Name { get; set; }
     }
 }
